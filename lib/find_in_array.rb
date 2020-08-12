358817e1d3f38cb70a629d3e0c4a2e41b615f38b
ZHENGJIAN LIU <comezheng@gmail.com>
2020-08-12 00:29:17 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,7 +1,3 @@
 def find_element_index(array, value_to_find)
-  count = 0
-  while count < array.length do
-    # comparison code will go here
-    count += 1
-  end
+  array.index(value_to_find)
 end
