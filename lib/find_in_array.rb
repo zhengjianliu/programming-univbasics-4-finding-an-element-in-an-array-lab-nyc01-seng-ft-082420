84138b1c808882aabcd0760075d92653df2fd12e
ZHENGJIAN LIU <comezheng@gmail.com>
2020-08-12 00:28:12 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -3,6 +3,8 @@ def find_element_index(array, value_to_find)
   while counter < array.length do
     if array[counter] == value_to_find
       return array[counter]
+    else if array[counter] != value_to_find
+      counter += 1
     else
       return nil
     end
